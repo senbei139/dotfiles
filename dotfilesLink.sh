d22ff9e98b4252cfb6f77b83a1f29784e63efa2a
senbei139 <senbei139@gmail.com>
2020-02-04 22:05:23 +0900

    add symbolic link setting to shellscript

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
 ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
