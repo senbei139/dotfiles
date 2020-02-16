45c78db6d36064668aa8c22c6690fe2f76580033
senbei139 <senbei139@gmail.com>
2020-02-17 00:32:58 +0900

    refactoring

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,4 @@ ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.agignore ~/.agignore
