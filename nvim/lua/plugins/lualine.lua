5747a5f37eaf1484c912a768a409b9b35f160bd6
senbei139 <senbei139@gmail.com>
2024-08-28 23:08:59 +0900

    update lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,8 +6,8 @@ return {
       return {
         require('lualine').setup {
           options = {
-            icons_enabled = true,
-            theme = 'auto',
+            icons_enabled = false,
+            theme = 'onedark',
             component_separators = { left = '|', right = '|'},
             section_separators = { left = '', right = ''},
             disabled_filetypes = {},
