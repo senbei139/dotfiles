4aa6cbc9ef1551b91ae5ead480848d414bbec50f
senbei139 <senbei139@gmail.com>
2024-11-24 17:37:57 +0900

    add alias code for wezterm

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -12,3 +12,4 @@ ln -sf ~/dotfiles/nvim/lua/plugins/*.lua ~/.config/nvim/lua/plugins/
 ln -sf ~/dotfiles/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.json
 sudo ln -sf ~/dotfiles/tmux-border /usr/local/bin/tmux-border
 ln -sf ~/dotfiles/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
+ln -sf ~/dotfiles/wezterm/*.lua ~/.config/wezterm/
