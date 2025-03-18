0f59685fcef9cadfdd012b71fee6b549a5b46511
senbei139 <senbei139@gmail.com>
2025-03-19 00:16:13 +0900

    change keymaps for lazygit and lazydocker

--- nvim/lua/toggleterm.lua
+++ nvim/lua/toggleterm.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygitToggle()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua _lazydockerToggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygitToggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ld", "<cmd>lua _lazydockerToggle()<CR>", { noremap = true, silent = true })
