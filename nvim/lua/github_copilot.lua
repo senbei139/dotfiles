f92a19f481f361cff78be5661f0bfd98bd31d256
senbei139 <senbei139@gmail.com>
2025-04-25 02:11:04 +0900

    Add keymap for copilot prompts

--- nvim/lua/github_copilot.lua
+++ nvim/lua/github_copilot.lua
@@ -1,1 +1,2 @@
 vim.api.nvim_set_keymap("n", "<leader>cc", ":CopilotChat<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>cp", ":CopilotChatPrompts<CR>", { noremap = true, silent = true })
