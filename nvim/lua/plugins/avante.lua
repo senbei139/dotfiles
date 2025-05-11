6c426e2bdd329c87366c58e90badf91c64af0d15
senbei139 <senbei139@gmail.com>
2025-05-11 13:38:01 +0900

    uncomment avante and update lspconfig

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,47 +1,47 @@
 return {
---  {
---    "yetone/avante.nvim",
---    event = "VeryLazy",
---    lazy = false,
---    version = false,
---    opts = {
---      provider = "copilot", --"claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot"
---      auto_suggestions_provider = "copilot",
---
---      -- 動作設定
---      behaviour = {
---        auto_suggestions = false,
---        auto_set_highlight_group = true,
---        auto_set_keymaps = true,
---        auto_apply_diff_after_generation = false,
---        support_paste_from_clipboard = false,
---        minimize_diff = true,
---      },
---
---      -- ウィンドウ設定
---      windows = {
---        position = "right",  -- サイドバーの位置
---        wrap = true,        -- テキストの折り返し
---        width = 30,         -- サイドバーの幅
---        -- その他の詳細設定は省略
---      },
---      copilot = {
---        model = "gpt-4o-2024-05-13",
---        -- model = "gpt-4o-mini",
---        max_tokens = 4096,
---      },
---    },
---    build = "make",
---    -- 依存関係の設定
---    dependencies = {
---      -- 必須の依存関係
---      "nvim-treesitter/nvim-treesitter",
---      "stevearc/dressing.nvim",
---      "nvim-lua/plenary.nvim",
---      "MunifTanjim/nui.nvim",
---      -- オプションの依存関係
---      "hrsh7th/nvim-cmp",
---      "zbirenbaum/copilot.lua",
---    }
---  }
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "copilot", --"claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot"
+      auto_suggestions_provider = "copilot",
+
+      -- 動作設定
+      behaviour = {
+        auto_suggestions = false,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+        minimize_diff = true,
+      },
+
+      -- ウィンドウ設定
+      windows = {
+        position = "right",  -- サイドバーの位置
+        wrap = true,        -- テキストの折り返し
+        width = 30,         -- サイドバーの幅
+        -- その他の詳細設定は省略
+      },
+      copilot = {
+        model = "gpt-4o-2024-05-13",
+        -- model = "gpt-4o-mini",
+        max_tokens = 4096,
+      },
+    },
+    build = "make",
+    -- 依存関係の設定
+    dependencies = {
+      -- 必須の依存関係
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      -- オプションの依存関係
+      "hrsh7th/nvim-cmp",
+      "zbirenbaum/copilot.lua",
+    }
+  }
 }
--- nvim/lua/plugins/lspconifg.lua
+++ nvim/lua/plugins/lspconifg.lua
@@ -51,7 +51,15 @@ return {
       lspconfig.eslint.setup(eslint_opts)
       lspconfig.biome.setup{
         capabilities = capabilities,
-        cmd = { "npx", "biome", "lsp-proxy" }
+        cmd = { "npx", "biome", "lsp-proxy" },
+        on_new_config = function(new_config)
+          local pnpm = lspconfig.util.root_pattern("pnpm-lock.yml", "pnpm-lock.yaml")(vim.api.nvim_buf_get_name(0))
+          local cmd = { "npx", "biome", "lsp-proxy" }
+          if pnpm then
+            cmd = { "pnpm", "biome", "lsp-proxy" }
+          end
+          new_config.cmd = cmd
+        end,
       }
       lspconfig.svelte.setup{
         capabilities = capabilities,
