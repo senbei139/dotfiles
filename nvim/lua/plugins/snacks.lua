adcb4206e8bff5a5f6423a5128027be493368ad8
senbei139 <senbei139@gmail.com>
2025-12-14 01:27:05 +0900

    feat: add snacks.nvim plugin

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        enabled = true,
+        formats = {
+          key = function(item)
+            return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
+          end,
+        },
+        sections = {
+          { section = "terminal", cmd = "fortune | cowsay", hl = "header", padding = 1, indent = 8 },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Sessions", key = "s", section = "session" },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
+        },
+      },
+    },
+  }
+}
