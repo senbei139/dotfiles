cd0660e8876f519392bf004777ac708f66686dec
senbei139 <senbei139@gmail.com>
2026-02-16 00:07:59 +0900

    feat: 開発環境セットアップスクリプトにgh, ghqを追加

--- initilize.sh
+++ initilize.sh
@@ -1,3 +1,4 @@
+brew update
 sudo gem install cocoapods
 brew install the_silver_searcher
 brew install --cask wireshark
@@ -5,3 +6,4 @@ brew install --cask wezterm
 brew install zsh-syntax-highlighting
 brew install xwmx/taps/nb
 brew install fortune cowsay
+brew install gh ghq
